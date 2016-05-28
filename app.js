dfd607d3af9dc9c53b5561095d40632968a34ab3
Paul Davidson <paul.s.davidson26@gmail.com>
2016-05-28 11:27:25 +1000

    back to jade :(

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -71,7 +71,7 @@ var client = new Twitter({
    access_token_secret: process.env.DB_AccessTokenKey
 });
 
-var searchTerm = '#ausvotes -filter:retweet';
+var searchTerm = '#ausvotes';
 
 /* let's do a regular job */
 var CronJob = require('cron').CronJob;
